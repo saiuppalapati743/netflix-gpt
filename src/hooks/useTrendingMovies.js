a8c7828f0d04be7c43de964ae0b61c378b2082e7
saiUppa <saiuppalapati96@gmail.com>
2026-01-21 07:17:36 +0530

    added nowplaying,trending,popular and upcoming movie cards in browse page

---
+++ src/hooks/useTrendingMovies.js
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addTrendingMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+const useTrendingMovies = () => {
+  const dispatch = useDispatch();
+
+  const trendingMovies = useSelector((store) => store.movies.trendingMovies);
+
+  const getTrendingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/trending/all/week?page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    dispatch(addTrendingMovies(json.results));
+  };
+
+  useEffect(() => {
+    !trendingMovies && getTrendingMovies();
+  }, []);
+};
+
+export default useTrendingMovies;
